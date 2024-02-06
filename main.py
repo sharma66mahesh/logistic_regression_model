575f46b79b267a0da93263ff4d6c48fc46ef9d0b
sharma66mahesh <gauravpudel1990@gmail.com>
2024-02-06 13:23:34 +0545

    Add forward and backward propagation

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from utils.lr_utils import load_dataset
+from model.LogisticRegression import LogisticRegression
+import numpy as np
 
 train_dataset_path = 'datasets/train_catvnoncat.h5'
 test_dataset_path = 'datasets/test_catvnoncat.h5'
 
-train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes = load_dataset(
+# load test and train data
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset(
     train_dataset_path=train_dataset_path, test_dataset_path=test_dataset_path)
 
-print(train_set_x_orig)
-print(train_set_y_orig)
-print(test_set_x_orig)
-print(test_set_y_orig)
-print(classes)
+# format train and test data
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+# normalize data
+train_set_x = train_set_x_flatten / 255
+test_set_x = test_set_x_flatten / 255
+
+regression_model = LogisticRegression(train_set_x, train_set_y, test_set_x,
+                                      test_set_y, classes, num_iterations=100, learning_rate=0.01, print_cost=False)
+
+print(regression_model.propagate())
--- model/LogisticRegression.py
+++ model/LogisticRegression.py
@@ -1,4 +1,56 @@
+import numpy as np
+from utils.lr_utils import sigmoid
+
+
 class LogisticRegression:
-  def __init__(self, train_dataset, test_dataset):
-    self.train_dataset = train_dataset
-    self.test_dataset = test_dataset
+    """
+    Train, test and predict using logistic regression model
+    """
+
+    def __init__(self, train_dataset_x: np.ndarray, train_dataset_y: np.ndarray,
+                 test_dataset_x: np.ndarray, test_dataset_y: np.ndarray, classes: str,
+                 num_iterations: int, learning_rate: float, print_cost: bool):
+        self.train_set_x = train_dataset_x
+        self.train_set_y = train_dataset_y
+        self.test_set_x = test_dataset_x
+        self.test_set_y = test_dataset_y
+
+        self.classes = classes
+        self.num_iterations = num_iterations
+        self.learning_rate = learning_rate
+        self.print_cost = print_cost
+
+        self.n_x = self.train_set_x.shape[0]  # no of input features
+        self.m = train_dataset_x.shape[1]  # no of training data
+
+        self.w = np.zeros([self.n_x, 1])
+        self.b = 0.0
+
+    def propagate(self):
+        """Run forward and backward propagation once
+
+        Returns:
+            `({"dw": dw, "db" db}, cost)`: tuple containing a dict of gradients `dw` & `db` and cost
+        """
+        z = np.dot(self.w.T, self.train_set_x) + self.b
+        a = sigmoid(z)
+
+        # Introduced so that we don't run into log(0)
+        epsilon = 1e-5
+
+        loss = -self.train_set_y * \
+            np.log(a + epsilon) - (1 - self.train_set_y) * \
+            np.log(1 - a + epsilon)
+        cost = np.sum(loss) / self.m
+
+        dz = a - self.train_set_y
+        dw = np.dot(self.train_set_x, dz.T) / self.m
+        db = np.sum(dz) / self.m
+
+        assert (dw.shape == self.w.shape)
+        assert (db.dtype == float)
+        assert (cost.dtype == float)
+        
+        grads = {"dw": dw, "db": db}
+
+        return grads, cost
